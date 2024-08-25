1550e8d603d955f074205e1cdaa972c4789139cb
kkihwan22 <jinkh97@gmail.com>
2024-08-26 01:11:31 +0900

    간단한 멀티모듈 테스트 코드

---
+++ apps/simple/src/test/java/SimpleTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleTest {
+    @Test
+    void onePlusTwoShouldReturnThree() {
+        // when
+        int result = 1 + 2;
+
+        // then
+        assertEquals(result, 3);
+    }
+}
